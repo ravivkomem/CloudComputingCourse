f39dbc271f1e1a80833742c4b6fbae4c2a71f79b
raviv <ravivkomem@gmail.com>
2021-05-02 20:21:41 +0300

    Added cars data
    Signed-off-by: raviv <ravivkomem@gmail.com>

---
+++ Website/src/Data/CarsData.js
@@ -0,0 +1,25 @@
+import MaseratiLevanteImg from '../Images/Cars/MaseratiLevante.jpg'
+import PaganiHuayraRoadsterImg from '../Images/Cars/PaganiHuayraRoadster.jpg'
+import MercedesBenzClassS from '../Images/Cars/MercedesBenzClassS.jpg'
+
+export const carsData = [
+    {
+        id: 1,
+        name: "Maserati Levante",
+        price: 300.00,
+        img: MaseratiLevanteImg,
+    },
+    {
+        id: 2,
+        name: "Pagani Huayra Roadster",
+        price: 367.67,
+        img: PaganiHuayraRoadsterImg,
+    },
+    {
+        id: 3,
+        name: "Merceds Benz S Class",
+        price: 2000000,
+        img: MercedesBenzClassS,
+    },
+];
+
