f295c8158cf97b1735e8cc5a34f43089b8172bec
Lior <123liorkauffman@gmail.com>
2021-05-03 15:59:01 +0300

    ss
    Signed-off-by: Lior <123liorkauffman@gmail.com>

--- Website/src/Data/CarsData.js
+++ Website/src/Data/CarsData.js
@@ -1,18 +1,21 @@
 import MaseratiLevanteImg from '../Images/Cars/MaseratiLevante.jpg'
 import PaganiHuayraRoadsterImg from '../Images/Cars/PaganiHuayraRoadster.jpg'
 import MercedesBenzClassS from '../Images/Cars/MercedesBenzClassS.jpg'
-
+import RollsRoyceImg from '../Images/Cars/rolls-royce-phantom.jpg'
+import BugattiImg from '../Images/Cars/bugatti-chiron.jpg'
+import FerrariImg from '../Images/Cars/Ferrari_SF90.jpg'
+import TeslaImg from '../Images/Cars/tesla-model3.jpg'
 export const carsData = [
     {
         id: 1,
         name: "Maserati Levante",
-        price: 300.00,
+        price: 83385,
         img: MaseratiLevanteImg,
     },
     {
         id: 2,
         name: "Pagani Huayra Roadster",
-        price: 367.67,
+        price: 3500000,
         img: PaganiHuayraRoadsterImg,
     },
     {
@@ -21,5 +24,29 @@ export const carsData = [
         price: 2000000,
         img: MercedesBenzClassS,
     },
+    {
+        id: 4,
+        name: "Rolls Royce Phantom",
+        price: 569750,
+        img: RollsRoyceImg,
+    },
+    {
+        id: 5,
+        name: "Bugatti Chiron",
+        price: 3000000,
+        img: BugattiImg,
+    },
+    {
+        id: 6,
+        name: "Ferrari SF90",
+        price: 500000,
+        img: FerrariImg,
+    },
+    {
+        id: 7,
+        name: "Tesla Model 3",
+        price: 38690,
+        img: TeslaImg,
+    }
 ];
 
